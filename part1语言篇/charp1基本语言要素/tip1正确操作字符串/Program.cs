94d4822706a0ff3cb8732710f14170d5012fd1a2
yhhno <516061849@qq.com>
2015-12-25 10:16:32 +0800

    1:initialization

---
+++ part1语言篇/charp1基本语言要素/tip1正确操作字符串/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tip1正确操作字符串
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
